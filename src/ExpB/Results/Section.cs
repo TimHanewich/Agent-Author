e5c919825a937483c6675235668cf7c36b43ba1d
Tim Hanewich <tahanewich@live.com>
2026-01-04 08:45:39 -0500

    Section creation

--- src/ExpB/Results/Section.cs
+++ src/ExpB/Results/Section.cs
@@ -4,10 +4,14 @@ namespace AgentAuthor.ExpB.Results
 {
     public class Section
     {
+        public string Heading {get; set;} //the title
+        public string Goal {get; set;} //the stated goal of this section, as planned by the contributor
         public string Content {get; set;}
         
         public Section()
         {
+            Heading = string.Empty;
+            Goal = string.Empty;
             Content = string.Empty;
         }
     }
